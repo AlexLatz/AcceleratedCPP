9e4e8c34c95121b020cb3a657838a875affe55b8
AlexLatz <alex@latz.tech>
2021-04-09 09:36:25 -0400

    finished grammar project

--- Chapter7/Grammar.cpp
+++ Chapter7/Grammar.cpp
@@ -2,8 +2,18 @@
 
 int main() 
 {
+    std::srand(std::time(0));
     Grammar grammar;
     readIn(grammar);
+    while (true) 
+    {
+        std::cout << "Enter a category, -1 to exit: ";
+        std::string category;
+        std::cin >> category;
+        if (category == "-1") break;
+        else std::cout << parseCategory(grammar, category) << std::endl;
+    }
+
 }
 
 bool isCategory(std::string s) 
@@ -14,7 +24,7 @@ bool isCategory(std::string s)
 std::string parseCategory(Grammar& grammar, std::string category)
 {
     std::vector<Rule> rules = grammar.at(category);
-    Rule rule = rules[rand() % rules.size()];
+    Rule rule = rules[std::rand() % rules.size()];
     std::string words;
     for (std::string s : rule) {
         if (isCategory(s)) words += parseCategory(grammar, s);
@@ -26,16 +36,18 @@ std::string parseCategory(Grammar& grammar, std::string category)
 
 void readIn(Grammar& grammar)
 {
-    while (std::cin)
+    while (true)
     {
-        std::cout << "Enter a category: ";
+        std::cout << "Enter a category, -1 to exit: ";
         std::string category;
         std::cin >> category;
+        if (category == "-1") break;
         std::cout << "Enter rules, -1 to exit: ";
         while (true) 
         {
             std::string line;
             std::getline(std::cin, line);
+            if (line.empty()) continue;
             if (line == "-1") break;
             Rule rule = split(line);
             grammar[category].push_back(rule);
@@ -48,8 +60,13 @@ Rule split(std::string line)
 {
     Rule rule;
     int index = 0;
-    for (int i = 0; i < line.size(); i++) {
-        if (std::isspace(line[i])) rule.push_back(line.substr(index, index-i));
+    for (int i = 0; i <= line.size(); i++) {
+        if (std::isspace(line[i]) || i == line.size()) {
+            if (!line.substr(index, i-index).empty()) {
+                rule.push_back(line.substr(index, i-index));
+                index = i+1;
+            }
+        }
     }
     return rule;
 }
